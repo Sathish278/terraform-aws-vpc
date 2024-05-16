102980ad62eb482544d31ae21378c00bcc7dad52
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-05-16 18:06:52 +0530

    .gitignore

--- main.tf
+++ main.tf
@@ -14,14 +14,21 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc-main"
+  }
+
 }
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "public"
+    Name = "sub-public"
   }
 }
 resource "aws_subnet" "private" {
@@ -29,7 +36,7 @@ resource "aws_subnet" "private" {
   cidr_block = "10.0.2.0/24"
 
   tags = {
-    Name = "private"
+    Name = "sub-private"
   }
 }
 resource "aws_subnet" "database" {
@@ -37,7 +44,7 @@ resource "aws_subnet" "database" {
   cidr_block = "10.0.3.0/24"
 
   tags = {
-    Name = "database"
+    Name = "sub-database"
   }
 }
 resource "aws_internet_gateway" "gw" {
@@ -50,38 +57,34 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
   tags = {
-    Name = "public"
+    Name = "rt-public"
   }
 }
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "10.0.2.0/24"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
   tags = {
-    Name = "private"
+    Name = "rt-private"
   }
 }
 resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "10.0.3.0/24"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
 
   tags = {
-    Name = "database"
+    Name = "rt-database"
   }
+}
+
+resource "aws_eip" "nat-eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+
+
 }
 
 resource "aws_route_table_association" "sub-public" {
@@ -98,14 +101,32 @@ resource "aws_route_table_association" "sub-database" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat-eip.id
   subnet_id     = aws_subnet.public.id
 
   tags = {
-    Name = "gw NAT"
+    Name = "gw-NAT"
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route" "private_route_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route" "database_route_nat" {
+  route_table_id         = aws_route_table.database.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
